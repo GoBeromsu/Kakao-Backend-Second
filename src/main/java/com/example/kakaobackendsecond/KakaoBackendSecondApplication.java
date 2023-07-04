7de8f56ae13f0d6218a624c8b1d8473bc1679093
goberomsu <gobeumsu@gmail.com>
2023-07-04 17:09:32 +0900

    Feat : 프로젝트 초기 설정
    - 의존성 설치

---
+++ src/main/java/com/example/kakaobackendsecond/KakaoBackendSecondApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakaobackendsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoBackendSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoBackendSecondApplication.class, args);
+	}
+
+}
